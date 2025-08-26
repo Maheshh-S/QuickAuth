9ee0d793f82af7444e62640355bc846eab586096
Mahesh S <dev3mahesh@gmail.com>
2025-08-26 22:14:44 +0530

    Connected DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb://localhost:27017").then(()=>{
+    console.log("yes connected")
+})
 
 const app = express();
 
 app.listen(3000 , () => {
-    console.log("server in 3000")
+    console.log("serverr in 3000")
 })
