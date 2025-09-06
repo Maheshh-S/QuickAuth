536447097c940c005218a7b0ba64fba6d50671f1
Mahesh S <dev3mahesh@gmail.com>
2025-09-06 18:57:22 +0530

    created Signin api route

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import User from "../models/userModel";
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post("/signup", async (req: Request, res: Response , next) => {
@@ -30,5 +31,57 @@ const hasedPass = bcryptjs.hashSync(password,10);
 
 
 });
+
+
+router.post('/signin' , async (req,res,next) => {
+    
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+        
+        const validUser = await User.findOne({email});
+
+        if(!validUser) return res.status(404).json({
+            message : " invaied user or user not found"
+        })
+      
+       // @ts-ignore
+        const vaildPassword = bcryptjs.compareSync(password, validUser.password)
+
+
+        if(!vaildPassword) return res.status(401).json({
+            message : " invaild credentials"
+        })
+        
+//change here evn ehile hosting
+//@ts-ignore
+
+const{password: hasedPass, ...rest} = validUser._doc;
+
+        const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET || "fallback_secret",
+        {expiresIn : "1h"}
+        );
+
+        res.cookie("access_token", token, 
+            {httpOnly: true},
+            
+        );
+
+        res.status(200).json({
+            message : "LoginSuccessfull",
+            token,
+            rest
+            // access_token
+        })
+
+    } catch (error) {
+        next(error);
+    }
+
+})
+
+
 
 export default router;
